1725848df92e712a82897ea1d516beb7baaed567
AlexMato <matontialessandro0@gmail.com>
2020-03-20 16:15:09 +0100

    fine ex1

---
+++ main.js
@@ -0,0 +1,19 @@
+// raffronto tra numeri
+alert('benvenuto nel giochino del numerino')
+
+//inserimento primo numero
+var primoNum = parseInt(prompt("fate il vostro gioco ed inserite il primo numero"));
+
+//inserimento secondo numero
+var secondoNum = parseInt(prompt("adesso tocca a te là dietro, si dico a te, fai il tuo gioco"));
+
+//var del vincitore
+var vincitore;
+//scelta numero maggiore
+if (primoNum > secondoNum) {
+  vincitore = primoNum;
+} else if (primoNum < secondoNum){
+  vincitore = secondoNum;
+}
+//output numero maggiore
+document.getElementById('ex-1').innerHTML= "il vincitore è il tizio che ha puntato " + vincitore;
